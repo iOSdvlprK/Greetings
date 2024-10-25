7036cc3d97c2796d0861567c4eb27824297d957a
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-10-25 20:48:35 +0900

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by joe on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
