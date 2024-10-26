8f9dd8f02c0c144cd575cc535bac66995d985c4f
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-10-26 22:52:39 +0900

    starting point with code redundancy

--- Greetings/ContentView.swift
+++ Greetings/ContentView.swift
@@ -9,11 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Hello there!")
+                .fontWeight(.semibold)
+                .padding()
+                .foregroundStyle(Color.white)
+                .background(Color.green.opacity(0.4))
+                .cornerRadius(20.0)
+                .shadow(color: .green, radius: 5, x: 10, y: 10)
+            
+            Text("Hi, welcome to Greetings!")
+                .fontWeight(.semibold)
+                .padding()
+                .foregroundStyle(Color.white)
+                .background(Color.gray.opacity(0.4))
+                .cornerRadius(20.0)
+                .shadow(color: .gray, radius: 5, x: 10, y: 10)
+            
+            Text("Are you ready to")
+                .fontWeight(.semibold)
+                .padding()
+                .foregroundStyle(Color.white)
+                .background(Color.yellow.opacity(0.4))
+                .cornerRadius(20.0)
+                .shadow(color: .yellow, radius: 5, x: 10, y: 10)
+            
+            Text("start exploring?")
+                .fontWeight(.semibold)
+                .padding()
+                .foregroundStyle(Color.white)
+                .background(Color.red.opacity(0.4))
+                .cornerRadius(20.0)
+                .shadow(color: .red, radius: 5, x: 10, y: 10)
+            
+            Text("Boom.")
+                .fontWeight(.semibold)
+                .padding()
+                .foregroundStyle(Color.white)
+                .background(Color.purple.opacity(0.4))
+                .cornerRadius(20.0)
+                .shadow(color: .purple, radius: 5, x: 10, y: 10)
         }
         .padding()
     }
