3c4236e5f5dc5f632d45ce674fbc6207337765bb
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-11-04 21:35:22 +0900

    support iPad

--- Greetings/Views/MainView.swift
+++ Greetings/Views/MainView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 // Portrait → Compact width, regular height
+// iPad → Regular width, regular height
 
 struct MainView: View {
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
     @Environment(\.verticalSizeClass) var verticalSizeClass
     
+    var isPortraitPhone: Bool {
+        horizontalSizeClass == .compact && verticalSizeClass == .regular
+    }
+    var isIPad: Bool {
+        horizontalSizeClass == .regular && verticalSizeClass == .regular
+    }
+    
     var body: some View {
         // Portrait mode?
-        if horizontalSizeClass == .compact && verticalSizeClass == .regular {
+        if isPortraitPhone || isIPad {
             GreetingsView()
         } else {
             // Landscape mode?
