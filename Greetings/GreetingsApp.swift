90f3d4b188b7bed205823c5a00b2e8d5b656fa8b
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-11-06 23:34:43 +0900

    change language and persist using AppStorage

--- Greetings/GreetingsApp.swift
+++ Greetings/GreetingsApp.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 @main
 struct GreetingsApp: App {
-    @State private var language: String = "en"
-    @State private var languageDirection: String = LEFT_TO_RIGHT
+    @AppStorage("language") var language: String = "en"
+    @AppStorage("languageDirectionString") var languageDirectionString: String = LEFT_TO_RIGHT
+    
+    var layoutDirection: LayoutDirection {
+        languageDirectionString == LEFT_TO_RIGHT ? .leftToRight: .rightToLeft
+    }
     
     var body: some Scene {
         WindowGroup {
-            MainView(language: $language, layoutDirectionString: $languageDirection)
+            MainView(language: $language, layoutDirectionString: $languageDirectionString)
+                .environment(\.locale, Locale(identifier: language))
+                .environment(\.layoutDirection, layoutDirection)
         }
     }
 }
